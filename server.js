6a53936d90d3fad79b66a7a275b67196ad0d903c
Sophie Constantin <scsophieconstantin@gmail.com>
2023-09-22 18:54:13 +0200

    JWT Token

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' });
+const { checkUser } = require('./middleware/auth.middleware');
 require('./config/db');
 const app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//jwt
+app.get('*', checkUser);
 
 // routes
 app.use('/api/user', userRoutes);
